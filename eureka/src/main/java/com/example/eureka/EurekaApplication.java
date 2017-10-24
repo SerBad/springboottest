5b9f84b23d650350b02b61c9535d7825bb6165ab
zhoutingjie <zhoutjcc@msn.cn>
2017-10-24 10:57:43 +0800

    升级pom文件

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author zhou
+ */
 @SpringBootApplication
 @EnableEurekaServer //通过@EnableEurekaServer启动一个服务注册中心给其他应用使用
 public class EurekaApplication {
